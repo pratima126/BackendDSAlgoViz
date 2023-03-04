748f447a41a5fb221625364b45ccac7f81dcf226
pratima126 <pratima.pande12@gmail.com>
2023-03-04 20:39:23 +0545

    next commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+const loginForm = document.getElementById('login-form')
+
+const URL = 'http://localhost:3000'
+const SessionKey = 'Algo_Session'
+
+/**
+ * Handle Login
+ */
+loginForm.addEventListener('submit', async (e) => {
+	e.preventDefault()
+	const email = document.getElementById('email').value
+	const password = document.getElementById('password').value
+
+	const reqParams = {
+		email,
+		password
+	}
+
+	try {
+		 const response = await fetch(`${URL}/api/employee/store`, {
+			method: "POST", 
+			 mode: "no-cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(reqParams), 
+		});
+
+		if(response) {
+			/**
+			 * Route user to another authenticated page
+			 */
+			localStorage.setItem(SessionKey,email,password, response?.data?.token)
+			window.location.href = 'index.html';
+		}
+	
+	} catch(error) {
+		// Show error message
+    console.log('Error: ', error.message)
+	}
+
+})
